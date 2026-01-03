547d30c26ea9c939d2c9bfb24b7d068676cd3230
Goldy <66202304+THEGOLDENPRO@users.noreply.github.com>
2026-01-03 23:26:47 +0000

    feat: add typer cli and `--project` and `--platform` options

---
+++ suap/main.py
@@ -0,0 +1,24 @@
+from typing import Annotated
+
+from enum import StrEnum
+from typer import Option, Typer
+
+app = Typer()
+
+class ProjectType(StrEnum):
+    CARGO = "cargo"
+
+class Platform(StrEnum):
+    LINUX = "linux"
+    WINDOWS = "windows"
+    MACOS = "macos"
+
+@app.command()
+def main(
+    project: Annotated[ProjectType, Option()],
+    platform: Annotated[Platform, Option()]
+):
+    print("WIP!")
+
+if __name__ == "__main__":
+    app()
